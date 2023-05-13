0b29eae06b38691c6ab6cf907ff204dd36c97cba
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-05-13 14:01:08 -0500

    ProductTag.js
    Added ProductTag model for database integration.

--- models/dist/ProductTag.dev.js
+++ models/dist/ProductTag.dev.js
@@ -32,9 +32,30 @@ function (_Model) {
   }
 
   return ProductTag;
-}(Model);
+}(Model); // define columns
 
-ProductTag.init({// define columns
+
+ProductTag.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id'
+    }
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+    }
+  }
 }, {
   sequelize: sequelize,
   timestamps: false,
